32b2946b096d6f1072e1fb8064dc1db473e2f83a
Waqas <waqas@eraofecom.org>
2026-06-18 13:55:36 +0500

    Search params fixed

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import { pageview } from "@/lib/gtag";
+import { pageview } from "@/lib/analytics";
 
 export default function GoogleAnalytics() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (!pathname) return;
 
-    const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : "");
-
-    pageview(url);
-  }, [pathname, searchParams]);
+    pageview(pathname);
+  }, [pathname]);
 
   return null;
 }
