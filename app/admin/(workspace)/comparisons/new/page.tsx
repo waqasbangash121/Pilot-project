bc2b6f63a457706eeb853d0068a96bef019a588a
Waqas Hanif Khan <waqasbangash7@gmail.com>
2026-06-29 19:21:01 +0500

    feat(comparisons): add comparison creation page

---
+++ app/admin/(workspace)/comparisons/new/page.tsx
@@ -0,0 +1,5 @@
+import { ManagedContentEditorForm } from "@/components/managed-content-editor-form";
+
+export default function NewComparisonPage() {
+  return <ManagedContentEditorForm type="comparison" />;
+}
